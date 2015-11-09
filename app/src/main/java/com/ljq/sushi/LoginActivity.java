b76ba0a57c368f18bf456a37d8d5402ad388863d
user.email <992807043@qq.com>
2015-11-09 22:38:27 +0800

    登录注册模块初步实现

--- app/src/main/java/com/ljq/sushi/LoginActivity.java
+++ app/src/main/java/com/ljq/sushi/LoginActivity.java
@@ -1,60 +1,69 @@
 package com.ljq.sushi;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.ljq.sushi.Util.Validate;
+import com.ljq.sushi.Util.Utilities;
 
 
 public class LoginActivity extends ActionBarActivity {
 
+    private TextInputLayout usernameWrapper = null;
+    private TextInputLayout passwordWrapper = null;
+    private Button loginBtn = null;
+    private Button registBtn=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final TextInputLayout usernameWrapper=(TextInputLayout)findViewById(R.id.usernameWrapper);
-        final TextInputLayout passwordWrapper=(TextInputLayout)findViewById(R.id.passwordWrapper);
-        final Button btn=(Button)findViewById(R.id.btn_login);
+        usernameWrapper=(TextInputLayout)findViewById(R.id.usernameWrapper);
+        passwordWrapper=(TextInputLayout)findViewById(R.id.passwordWrapper);
+        loginBtn=(Button)findViewById(R.id.btn_login);
+        registBtn= (Button) findViewById(R.id.btn_regist);
 
-        usernameWrapper.setHint("请输入帐号");
-        passwordWrapper.setHint("请输入密码");
+        usernameWrapper.setHint("请输入帐号(adm)");
+        passwordWrapper.setHint("请输入密码(adm)");
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hideKeyboard();
+                //hideKeyboard();
+                Utilities.hideKeyboard(LoginActivity.this);
 
-                String username=usernameWrapper.getEditText().getText().toString();
-                String password=passwordWrapper.getEditText().getText().toString();
-                if(!Validate.isEmail(username)){
-                    usernameWrapper.setError("Not a valid emial addres!");
-                }else if(!Validate.isPassword(password)){
-                    passwordWrapper.setError("Not a valid password!");
-                }else{
+                String username = usernameWrapper.getEditText().getText().toString();
+                String password = passwordWrapper.getEditText().getText().toString();
+                if (!username.equals("adm")) {
+                    usernameWrapper.setError("帐号是“adm");
+                } else if (!password.equals("adm")) {
+                    passwordWrapper.setError("密码是”adm");
+                } else {
                     usernameWrapper.setErrorEnabled(false);
                     passwordWrapper.setErrorEnabled(false);
                     doLogin();
                 }
             }
         });
+
+        registBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void hideKeyboard(){
-        View view = getCurrentFocus();
-        if(view!=null){
-            ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).
-                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-    }
+
 
     public void doLogin(){
         Toast.makeText(this,"登录成功",Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/ljq/sushi/test.java
+++
@@ -1,7 +0,0 @@
-package com.ljq.sushi;
-
-/**
- * Created by Administrator on 2015/11/8.
- */
-public class test {
-}
