128d83c6143ccc474e32bf3628472e2a223844f7
user.email <992807043@qq.com>
2015-11-09 23:48:57 +0800

    登录注册模块初步实现

---
+++ app/src/main/java/com/ljq/sushi/RegisterActivity.java
@@ -0,0 +1,44 @@
+package com.ljq.sushi;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ljq.sushi.Util.Utilities;
+
+
+public class RegisterActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setTitle("注册");
+        setContentView(R.layout.activity_register);
+        //ActionBar actionBar=getActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("注册");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                Utilities.hideKeyboard(RegisterActivity.this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
